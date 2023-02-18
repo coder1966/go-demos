1d7f0d7b55166e9574a4d1ee01db42aa7ca306a1
coder1966 <jqm_112906505@qq.com>
2023-02-18 09:04:27 +0800

    only save

--- 106panicrecover/main.go
+++ 106panicrecover/main.go
@@ -6,13 +6,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	err := sub()
 
 	fmt.Println("sub() 回来了", err)
-	// time.Sleep(time.Millisecond * 200000)
+	// time.Sleep(time.Millisecond * 10000)
 }
 
 func sub() (err error) {
@@ -24,17 +25,37 @@ func sub() (err error) {
 			fmt.Println("else errPanic", errPanic)
 		}
 	}()
-	sub01()
+
+	for {
+		fmt.Println("执行: ")
+
+		fmt.Println("返回 sub01() : ", sub01())
+		sub01()
+	}
+
 	// if err != nil {
 	// 	return fmt.Errorf("error: %v", err)
 	// }
-	return err
+	return nil
 }
 
-func sub01() {
-	sub02()
+func sub01() string {
+	for {
+		err := sub02()
+		if err == nil {
+			// Need get data to find backend errors.
+			err = nil
+			if err == nil {
+				return "正常返回"
+			}
+			// Sleep, retry.
+		}
+		time.Sleep(time.Second * 2)
+	}
+
 }
 
-func sub02() {
+func sub02() error {
 	panic("人为的恐慌")
+	return nil
 }
